22389c4c6a58225110706d7ca72af52695c7c88d
TomLucasakaTGeek <TanmayEmpowers@proton.me>
2025-06-29 13:22:45 +0530

    Adding Charts

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css';
 import logo from '../../assets/logo.png'
 import arrow from '../../assets/arrow_icon.png'
 import { CoinContext } from '../../context/CoinContext'
-
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   
@@ -48,10 +48,13 @@ const Navbar = () => {
 
   return (
     <div className='navbar'>
-      
+      <Link to={'/'}>
       <img src={logo} alt="" className='logo' />
+      </Link>
       <ul>
+        <Link to={'/'}>
         <li>Home</li>
+        </Link>
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
